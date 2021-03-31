dc7f095fd66b6c9cbd54368937f35d8fc4fa3085
ZWrld <54416396+Sup-Sup123@users.noreply.github.com>
2021-03-30 19:09:34 -0500

    Created 404.php

---
+++ pages/404.php
@@ -0,0 +1,7 @@
+<?php
+$pageTitle = '404 Not found | ZWrld';
+http_response_code(404);
+?>
+<article class="page page-notfound">
+    <h1>Sorry, this page or url/link you have tried to access is invalid, or does not exist! If you think this is a mistake or something is wrong please contact me ZWrld at zwrld@zwrld.me. Sorry for the inconvenience!</h1>
+</article>
