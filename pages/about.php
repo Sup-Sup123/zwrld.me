3e16e7fb35dce99f614bf51710bc02987819deda
ZWrld <54416396+Sup-Sup123@users.noreply.github.com>
2021-10-21 08:39:38 -0500

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -2,7 +2,7 @@
 <article class="page page-about">
     <h1>About ZWrld</h1>
     <p>
-            ZWrld is a 16 year old Musical Artist that lives in the United States. He was born in Colorado, but lives in Iowa. He loves to make music in his freetime, and he also enjoys playing xbox, especially GTA V
+            ZWrld is a 17 year old Musical Artist that lives in the United States. He was born in Colorado, but lives in Iowa. He loves to make music in his freetime, and he also enjoys playing xbox, especially GTA V
          <h2>ZWrld's Inspirations/Freetime</h2>
 
             ZWrld is/was inspired by Juice Wrld, XXXTentacion, Fall Out Boy, Black Veil Brides, Lil Peep, along with others. He likes to hangout with friends if they want to hangout. ZWrld spends most of his free time making music. He has always wanted to make music since he was a kid. But didn't want anyone he knew to find out (They found out anyway...), most of the time he ignores people, because he really doesn't socialize much.
